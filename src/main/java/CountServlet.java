ff034c423238ffd426c8e3d3b12e22f6a058d147
Bernie Esquivel <bernie.esquivel01@gmail.com>
2021-11-29 16:09:30 -0600

    feat: Servlet Exercises complete

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="CountServlet", urlPatterns="/count")
+class CountServlet extends HttpServlet {
+    int visits = 0;
+
+    protected void doGet(HttpServletRequest reqNum, HttpServletResponse resNum) throws IOException {
+        visits++;
+        resNum.getWriter().println(visits);
+
+    }
+}
